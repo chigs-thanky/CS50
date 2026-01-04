f320b294b4ac2c05e28c380956309781b6572d5b
chigs-thanky <chigsthanky@outlook.com>
2026-01-04 19:52:28 +0530

    NULL character

---
+++ C/nullcharacter.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+    // char arr[3] = "Hi!";//NULL character (\0) should be counted, otherwise it gives errors in memory
+    char arr[4] = "Hi!";
+    printf("%s\n", arr);
+}
